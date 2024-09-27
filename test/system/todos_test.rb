a0f70858bdc8c175a508cc5bd732f0002d71dc69
David Saitta <david.saitta@gmail.com>
2024-09-27 18:43:34 +0200

    add system tests

--- test/system/todos_test.rb
+++ test/system/todos_test.rb
@@ -7,37 +7,79 @@ class TodosTest < ApplicationSystemTestCase
 
   test "visiting the index" do
     visit todos_url
-    assert_selector "h1", text: "Todos"
+    assert_selector "h1", text: "todos"
+
+    within ".todo-list" do
+      assert_selector "li", count: Todo.count
+    end
   end
 
   test "should create todo" do
     visit todos_url
-    click_on "New todo"
+    fill_in "todo[title]", with: "New Todo\n" # \n is the Enter key
 
-    check "Completed" if @todo.completed
-    fill_in "Title", with: @todo.title
-    click_on "Create Todo"
-
-    assert_text "Todo was successfully created"
-    click_on "Back"
+    within ".todo-list" do
+      assert_selector "li", text: "New Todo"
+    end
   end
 
-  test "should update Todo" do
-    visit todo_url(@todo)
-    click_on "Edit this todo", match: :first
+  test "should update todo" do
+    visit todos_url
+    within ".todo-list" do
+      targetTodo = find("li", text: @todo.title).find("form")
+      targetTodo.double_click
+      assert_selector "li form input[type=text]", visible: true
 
-    check "Completed" if @todo.completed
-    fill_in "Title", with: @todo.title
-    click_on "Update Todo"
-
-    assert_text "Todo was successfully updated"
-    click_on "Back"
+      targetTodo.fill_in "todo[title]", with: "Edited Todo\n" # \n is the Enter key
+      assert_selector "li", text: "Edited Todo"
+    end
   end
 
-  test "should destroy Todo" do
-    visit todo_url(@todo)
-    click_on "Destroy this todo", match: :first
+  test "should destroy todo" do
+    visit todos_url
+    within ".todo-list" do
+      targetButton = find("li", text: @todo.title).hover.find("button.destroy",  visible: false)
+      targetButton.click
+      assert_no_selector "li", text: @todo.title
+    end
+  end
 
-    assert_text "Todo was successfully destroyed"
+  test "should clear completed todos" do
+    @todo.update_attribute!(:completed, true)
+
+    visit todos_url
+    find("button.clear-completed").click
+
+    within ".todo-list" do
+      assert_no_selector "li", text: @todo.title
+    end
+  end
+
+  test "should filter active todos" do
+    @todo.update_attribute!(:completed, true)
+    todo2 = todos(:two)
+    todo2.update_attribute!(:completed, false)
+
+    visit todos_url
+    find("a", text: "Active").click
+
+    within ".todo-list" do
+      assert_no_selector "li", text: @todo.title
+      assert_selector "li", text: todo2.title
+    end
+  end
+
+  test "should filter completed todos" do
+    @todo.update_attribute!(:completed, true)
+    todo2 = todos(:two)
+    todo2.update_attribute!(:completed, false)
+
+    visit todos_url
+    find("a", text: "Completed").click
+
+    within ".todo-list" do
+      assert_selector "li", text: @todo.title
+      assert_no_selector "li", text: todo2.title
+    end
   end
 end
